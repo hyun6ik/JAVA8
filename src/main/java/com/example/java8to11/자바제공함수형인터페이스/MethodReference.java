d07336b1dd8a14bb2934e67f2d46338c4d5a7da8
hyun6ik <fkanwm@naver.com>
2021-08-12 22:52:26 +0900

    [JAVA8-3] add : 메소드 레퍼런스

---
+++ src/main/java/com/example/java8to11/자바제공함수형인터페이스/MethodReference.java
@@ -0,0 +1,32 @@
+package com.example.java8to11.자바제공함수형인터페이스;
+
+import java.util.Arrays;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.function.UnaryOperator;
+
+public class MethodReference {
+
+    public static void main(String[] args) {
+        Greeting greeting = new Greeting();
+
+        Supplier<Greeting> newGreeting = Greeting::new;
+        Greeting greeting2 = newGreeting.get();
+
+        Function<String, Greeting> hyunsikGreeting = Greeting::new;
+        Greeting hyunsik = hyunsikGreeting.apply("hyunsik");
+
+        String[] names = {"hyunsik", "dongju", "whiteship"};
+        Arrays.sort(names, String::compareToIgnoreCase);
+        System.out.println(Arrays.toString(names));
+
+        UnaryOperator<String> hi = Greeting::hi;
+        String hiHyunsik = hi.apply("hyunsik");
+        UnaryOperator<String> hello = greeting::hello;
+        String hi2Hyunsik = hello.apply("hyunsik");
+
+        System.out.println("hyunsik = " + hyunsik.getName());
+        System.out.println("hiHyunsik = " + hiHyunsik);
+        System.out.println("hi2Hyunsik = " + hi2Hyunsik);
+    }
+}
