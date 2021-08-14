230b0357c2cdab92974bc5f2527da4c36fff1a16
hyun6ik <fkanwm@naver.com>
2021-08-14 22:54:41 +0900

    [JAVA8-5] add : Stream API

---
+++ src/main/java/com/example/java8to11/스트림/OnlineClass.java
@@ -0,0 +1,42 @@
+package com.example.java8to11.스트림;
+
+public class OnlineClass {
+
+    private Integer id;
+    private String title;
+    private boolean closed;
+
+    public OnlineClass(Integer id, String title, boolean closed) {
+        this.id = id;
+        this.title = title;
+        this.closed = closed;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public OnlineClass(Integer id) {
+        this.id = id;
+    }
+
+    public boolean isClosed() {
+        return closed;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setClosed(boolean closed) {
+        this.closed = closed;
+    }
+}
